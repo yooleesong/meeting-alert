f3e17aba86537a0a505b7c7aafce95d2e2a379ba
Yoolee Song <57004576+yooleesong@users.noreply.github.com>
2021-04-08 01:22:38 +0900

    first commit

---
+++ webhook.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+
+function webhook(){
+    var today = new Date();
+    var date = today.getDate();
+    var month = today.getMonth();
+    var week = ['일', '월', '화', '수', '목', '금', '토'];
+    var payload = {
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":star: 곧 조직 관리팀 일일 회의가 시작됩니다. :star:"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "@org-mgt \n오른쪽 버튼을 눌러 회의에 참석해 주세요~! :hugging_face:\n"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "행아웃 바로가기 >",
+                        "emoji": true
+                    },
+                    "value": "click_me_123",
+                    "url": "https://meet.google.com/dmx-vjic-xjx",
+                    "action_id": "button-action"
+                }
+            },
+            {
+                "type": "divider"
+            }
+        ]
+    }
+    payload = JSON.stringify(payload);
+    fetch(SLACK_WEBHOOK_URL,{method:'POST', body: payload}).then(function(response){
+        return response.json();
+    })
+}
+
+
+async function postWebhook(){
+    try {
+        webhook();
+    }catch (error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+postWebhook();
